472c3069b500d22239e45f6f04b44bd1961232e7
Andrea9808 <andrea.manigrasso98@gmail.com>
2024-01-16 09:54:01 +0100

    jambo

---
+++ src/components/Jambo.vue
@@ -0,0 +1,23 @@
+<script>
+    export default{
+        name: "Jambo"
+    }
+</script>
+
+<template>
+    <div class="row">
+        <div class="col-6">
+            <div class="text-jambo">
+
+            </div>
+        </div>
+
+        <div class="col-6">
+
+        </div>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+</style>
