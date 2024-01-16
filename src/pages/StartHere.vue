3973bb14e76f42ae013b8f547766c428453de46f
saraaffro <saraaffro@gmail.com>
2024-01-16 16:28:05 +0100

    feat: step to success responsive init

--- src/pages/StartHere.vue
+++ src/pages/StartHere.vue
@@ -1,17 +1,21 @@
 <!-- sara -->
 <script>
 import SliderStartHere from '../components/SliderStartHere.vue';
+import StepToSuccess from '../components/StepToSuccess.vue';
+
 export default {
-name: "StartHere",
-components:{
-    SliderStartHere,
-}
+    name: "StartHere",
+    components:{
+        SliderStartHere,
+        StepToSuccess
+    }
 }
 </script>
 
 <template>
     <div class="container-xl container-fluid-lg">
         <SliderStartHere/>
+        <StepToSuccess/>
     </div>
 </template>
 
