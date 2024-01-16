9ad43b5b610881fceb416d623d7e95805fb94b83
LeandroL18 <leandrolipani6@gmail.com>
2024-01-16 10:22:54 +0100

    icon_footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -7,7 +7,19 @@ export default{
 <template>
     <footer>
         <div class="container-xl container-fluid-lg">
-            Footer
+            <div>
+                <h2>Address</h2>
+                <ul>
+                    <li>382 NE 191st St # 87394 Miami, FL 33179-3899</li>
+                    <li>+1 (305) 547-9909 (9am - 5pm EST, Monday - Friday)</li>
+                    <li><a href="#">support@maxcoach.com</a></li>
+
+                    <i class="fa-brands fa-facebook"></i>
+                    <i class="fa-brands fa-twitter"></i>
+                    <i class="fa-brands fa-instagram"></i>
+                    <i class="fa-brands fa-linkedin"></i>
+                </ul>
+            </div>
         </div>
     </footer>
    
