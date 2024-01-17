c7d66cfea01934653abf0aca6996640494f3a3f1
LeandroL18 <leandrolipani6@gmail.com>
2024-01-17 16:27:27 +0100

    feat: sottopagine correlate in fase di elaborazione

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -10,54 +10,54 @@ export default{
         <div id="bg" class="container-fluid-lg">
             <div class="container-xl">
                 <div class="flex">
-                <div id="lista1">
-                    <h4>Address</h4>
-                    <ul>
-                        <li>382 NE 191st St # 87394 Miami, FL 33179-3899</li>
-                        <li>+1 (305) 547-9909 (9am - 5pm EST, Monday - Friday)</li>
-                        <li><a href="#">support@maxcoach.com</a></li>
-                        <li>
-                            <a href="#"><i class="fa-brands fa-facebook"></i></a>
-                            <a href="#"><i class="fa-brands fa-twitter"></i></a>
-                            <a href="#"><i class="fa-brands fa-instagram"></i></a>
-                            <a href="#"><i class="fa-brands fa-linkedin"></i></a>
-                        </li>
-                    </ul>
+                    <div id="lista1">
+                        <h4>Address</h4>
+                        <ul>
+                            <li>382 NE 191st St # 87394 Miami, FL 33179-3899</li>
+                            <li>+1 (305) 547-9909 (9am - 5pm EST, Monday - Friday)</li>
+                            <li><a href="#">support@maxcoach.com</a></li>
+                            <li>
+                                <a href="#"><i class="fa-brands fa-facebook"></i></a>
+                                <a href="#"><i class="fa-brands fa-twitter"></i></a>
+                                <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                                <a href="#"><i class="fa-brands fa-linkedin"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="flex">
+                        <div id="lista2">
+                            <h4>Explore</h4>
+                            <ul>
+                                <li><a href="/start-here">Start here</a></li>
+                                <li><a href="#">Blog</a></li>
+                                <li><a href="#">About us</a></li>
+                            </ul>
+                        </div>
+
+                        <div id="lista3">
+                            <ul>
+                                <li><a href="#">Success story</a></li>
+                                <li><a href="/Course">Courses</a></li>
+                                <li><a href="#">Contact us</a></li>
+                            </ul>
+                        </div>
+
+                        <div id="lista4">
+                            <h4>Information</h4>
+                            <ul>
+                                <li><a href="#">Membership</a></li>
+                                <li><a href="#">Purchase guide</a></li>
+                                <li><a href="#">Privacy policy</a></li>
+                                <li><a href="#">Terms of services</a></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
 
-                <div class="flex">
-                    <div id="lista2">
-                        <h4>Explore</h4>
-                        <ul>
-                            <li><a href="/start-here">Start here</a></li>
-                            <li><a href="#">Blog</a></li>
-                            <li><a href="#">About us</a></li>
-                        </ul>
-                    </div>
-
-                    <div id="lista3">
-                        <ul>
-                            <li><a href="#">Success story</a></li>
-                            <li><a href="/Course">Courses</a></li>
-                            <li><a href="#">Contact us</a></li>
-                        </ul>
-                    </div>
-
-                    <div id="lista4">
-                        <h4>Information</h4>
-                        <ul>
-                            <li><a href="#">Membership</a></li>
-                            <li><a href="#">Purchase guide</a></li>
-                            <li><a href="#">Privacy policy</a></li>
-                            <li><a href="#">Terms of services</a></li>
-                        </ul>
-                    </div>
+                <div id="center">
+                    <span>© 2020 Maxcoach. All Rights Reserved</span>
                 </div>
-            </div>
-
-            <div id="center">
-                <span>© 2020 Maxcoach. All Rights Reserved</span>
-            </div>
             </div>
            
         </div>
@@ -71,8 +71,8 @@ export default{
     #bg{
         background-color: $bianco-sporco;
         padding: 4% 1.5% 4% 0;
-        display: flex;
-        flex-direction: column;
+        // display: flex;
+        // flex-direction: column;
     }
     .flex{
         display: flex;
@@ -85,7 +85,6 @@ export default{
     }
 
     ul{
-        list-style-type: none;
         margin-left: -31px;
     }
 
@@ -122,7 +121,8 @@ export default{
     }
 
     #center{
-        margin: 50px auto 0;
+        margin: 50px 0 0 0;
+        text-align: center;
     }
 
 
