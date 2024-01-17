511f7d12eeebfb396fa6b687dde9eeac4a6ad37a
Andrea9808 <andrea.manigrasso98@gmail.com>
2024-01-17 10:51:26 +0100

    feat: enjoy component

--- src/components/EnjoyCompanionship.vue
+++ src/components/EnjoyCompanionship.vue
@@ -9,7 +9,7 @@
 <template>
     <div class="container-xl">
         <div class="row">
-            <div class="col-6">
+            <div class="col-12 col-lg-6">
                 <div class="image">
                     <div class="avatar-container">
                         <img class="avatar" src="/public/home-2-popup-video-poster.jpg" alt="video poster">
@@ -18,20 +18,20 @@
                 </div>
             </div>
 
-            <div class="col-6">
+            <div class="col-12 col-lg-6">
                 <div class="text">
                     <div class="title">
                         <span class="get_started">GET STARTED <span class="color-effortlessly">EFFORTLESSLY</span></span>
                     </div>
                     <div class="sub-title">
-                        <h2>ENJOY OUR</h2>
+                        <h2 class="enjoy">ENJOY OUR</h2>
                         <h2 class="color-companionship">COMPANIONSHIP</h2>
                     </div>
                     <div>
                         <p>Sneek peek into what has in store for you on our online teaching and mentoring portal. Seek for some hidden hints of early-bird rewards as well.</p>
                     </div>
-                    <div>
-                        <a href="#">Download free guidebook <span><i class="fa-solid fa-arrow-right"></i></span></a>
+                    <div class="link">
+                        <a href="#"><strong>Download free guidebook <span><i class="fa-solid fa-arrow-right"></i></span></strong></a>
                     </div>
                 </div>
             </div>
@@ -65,39 +65,82 @@
         cursor: pointer;
     }
 
-    .get_started{
-        color:red;
+    
+    
+}
+
+    .text{
+
+        padding: 50px;
+        .title{
+        .get_started{
+            font-size: 15px;
+            font-weight: 500;
+            text-transform: uppercase;
+            line-height: 2em;
+            letter-spacing: 2px;
+            color: #8C89A2;
+        }
+
+        .color-effortlessly{
+            font-weight: 700;
+            color: #3F3A64;
+        }
+    }
+
+
+    .sub-title{
+
+        margin-top: 10px;
+        .enjoy{
+            font-size: 48px;
+            color: #3F3A64;
+        }
+
+        .color-companionship{
+            font-weight: 400;
+            font-size: 35px;
+            color: $verde-acqua;
+        }
+
+    }
+
+    p{
+        margin-top: 20px;
     }
    
+    .link{
+        margin-top: 20px;
+        a {
+            color: $verde-acqua;
+            position: relative;
+            text-decoration: none;
+            display: inline-block;
+            border-bottom: 1px solid rgb(199, 194, 194);
+            transition: border-color 0.5s ease-in-out;
 
-    // a {
-    //         color: $verde-acqua;
-    //         position: relative;
-    //         text-decoration: none;
-    //         display: inline-block;
-    //         border-bottom: 1px solid rgb(199, 194, 194);
-    //         transition: border-color 0.5s ease-in-out;
+            &:before {
+                content: '';
+                position: absolute;
+                top: 100%;
+                left: 0;
+                width: 100%;
+                height: 1px;
+                background-color: $verde-acqua;
+                transform: scaleX(0);
+                transform-origin: right;
+                transition: transform 0.5s ease-in-out;
+            }
 
-    //         &:before {
-    //             content: '';
-    //             position: absolute;
-    //             top: 100%;
-    //             left: 0;
-    //             width: 100%;
-    //             height: 1px;
-    //             background-color: $verde-acqua;
-    //             transform: scaleX(0);
-    //             transform-origin: right;
-    //             transition: transform 0.5s ease-in-out;
-    //         }
-
-    //         &:hover {
-    //             border-color: transparent;
-    //             &:before {
-    //                 transform: scaleX(1);
-    //                 transform-origin: left;
-    //             }
-    //         }
-    //     }
-}
+            &:hover {
+                border-color: transparent;
+                &:before {
+                    transform: scaleX(1);
+                    transform-origin: left;
+                }
+            }
+        }
+    }
+    }
+    
 </style>
